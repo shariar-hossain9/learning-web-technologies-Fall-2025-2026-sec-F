c6bbca999a428e54dda04f929732f736dd9616d6
shariar-hossain9 <shariar7161@gmail.com>
2025-12-10 14:23:22 +0600

    Lab PHP

---
+++ LAB2.1.php
@@ -0,0 +1,10 @@
+<?php
+$length = 2;
+$width = 5;
+$area = $length*$width;
+$perimeter = 2*($length + $width);
+echo "Length: $length<br>";
+echo "Width: $width<br>";
+echo "Area of Rectangle: $area<br>";
+echo "Perimeter of Rectangle: $perimeter<br>";
+?>
